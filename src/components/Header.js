a078c713e4992e763921457ab16589b569e31323
memematvaz <memematvaz@gmail.com>
2020-07-20 19:52:15 +0200

    header in pprogress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import logo from '../images/logo192.png'
 
 const Header = () => {
 
     return (
         <header className="header">
-            <h1>Jara Amaro psicóloga</h1>
+            <div className="header--logo-container">
+                <div className="header--logo-image-container container">
+                    <img src={logo} alt="logo Jara Amaro psicóloga"></img>
+                </div>
+                <h1 className="header--logo-title">Jara Amaro<br/>
+                                                    psicóloga</h1>
+            </div>
+            <div className="header--menu">
+               <i class="fas fa-bars container"></i>
+                <nav>
+                    <ul>
+                        <li>Consultas</li>
+                        <li>Testimonios</li>
+                        <li>Contacto</li>
+                        <li>Sobre mí</li>
+                       
 
+
+                    </ul>
+                </nav>
+
+            </div>
         </header>
     );
 };
