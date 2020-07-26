ab17ebb42a241559ff7b530ff5899f50eea27896
memematvaz <memematvaz@gmail.com>
2020-07-26 19:44:31 +0200

    hero bug

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ class Hero extends React.Component {
                 <div className="hero__background-image" src={ heroHome } alt="Jara en la clínica de psicología">
 
                     <div className="hero__text-container-pc container">
-                        <h2>¿Temes una terapia <span className="bold-title">interminable?</span></h2>
+                        <h2 className="hero__title">¿Temes una terapia <span className="bold-title">interminable?</span></h2>
                         <p>Sentirse mejor en menos de siete sesiones es posible.</p>
                         <p>Transforma tu vida en menos tiempo y libérate del malestar.</p>
                         <button className="button">¡Escríbeme!</button>
