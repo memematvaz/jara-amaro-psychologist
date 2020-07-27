d637c65592875d1a94bc0282b2765d683afe7ef3
memematvaz <memematvaz@gmail.com>
2020-07-27 19:20:21 +0200

    card details mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = (props) => {
     
     const toggleCard = () => {
         if (isCardOpen === "card__container"){
-            setIsCardOpen("header__hamburger-open")
+            setIsCardOpen("card-details__container")
         } else {
             setIsCardOpen("card__container")
         }
@@ -18,9 +18,7 @@ const Card = (props) => {
         <>
             <div id={ props.key } className={ isCardOpen }>
                 <div className="card__header">
-                    <div className="card-details__close-icon"  onClick={ toggleCard }>
-                        <i className="icon fas fa-times"></i> 
-                    </div>
+
                 
                     <div className="card__circle">
                         <i className={ props.icon }>
@@ -28,22 +26,23 @@ const Card = (props) => {
                     </div>
                     <h4 className="container">Consultas <br/>
                     { props.title }</h4>
+                    <div className="card-details__close-icon"  onClick={ toggleCard }>
+                        <i className="icon fas fa-times"></i> 
+                    </div>
                 </div>
                 <div className="card__text-container container">
                     <p className="card__text">{ props.text } </p>
                     <p className="card__text">{ props.price } </p>
                     <div className="read-more"  onClick={ toggleCard }>Leer más</div>
+                    
                 </div>
                 <div className="card-details__text-container container">
-                        <p>{ props.how } { props.time }</p>
+                        <p>{ props.how } <span className="card-details__mobile-no">{ props.time }</span></p>
                         <p>{ props.include }</p>
-                        <p>{ props.perfect }</p>
+                        <p className="card-details__mobile-no">{ props.perfect }</p>
                         <p>{ props.need }</p>
-                        <h5>{ props.slogan }</h5>
-                        <p>Solicita tu cita primera cita llamando al teléfono<span className="telephone"> 680 36 26 44</span><br/>
-                        o escríbeme rellenando el siguiente formulario:
-                    </p>
-                    <button className="button">¡Escríbeme!</button>
+                        <h5 className="card-details__mobile-no">{ props.slogan }</h5>
+                        <div className="read-less"  onClick={ toggleCard }>Leer menos</div>
                 </div>
             
             </div>
---
+++ src/components/Contact.js
@@ -0,0 +1,4 @@
+/*<p>Solicita tu cita primera cita llamando al teléfono<span className="telephone"> 680 36 26 44</span><br/>
+                        o escríbeme rellenando el siguiente formulario:
+                    </p>
+                    <button className="button">¡Escríbeme!</button>*/
