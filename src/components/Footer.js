a7699bd1aadfced2819b594bccd9454d5db68862
memematvaz <memematvaz@gmail.com>
2020-07-29 21:56:51 +0200

    footer mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logoInverted from '../images/logo-white.png'
 import { Link } from 'react-router-dom';
 
+import linkedin from '../images/linkedin.png'
+import doctoralia from '../images/doctoralia.png'
+import mundopsicologos from '../images/mundopsicologos.png'
 
 const Footer = () => {
 
@@ -18,19 +21,19 @@ const Footer = () => {
                 </div>
                 </Link>
                 <nav>
-                    <ul className="header__menu-list">
-                            <li><a href="https://www.linkedin.com/in/jara-amaro-mazaira-791b17108/">Linkedin</a></li>
+                    <ul className="footer-rrss">
+                            <li><a href="https://www.linkedin.com/in/jara-amaro-mazaira-791b17108/"> <img className="footer__logo-linkedin" src={ linkedin } alt="linkedin"></img></a></li>
                             
-                            <li><a href="https://www.doctoralia.es/jara-amaro-mazaira/psicologo/vigo">Doctoralia</a></li>
+                            <li><a href="https://www.doctoralia.es/jara-amaro-mazaira/psicologo/vigo"><img className="footer__logo-doctoralia" src={ doctoralia } alt="doctoralia"></img></a></li>
                         
-                            <li><a href="https://www.mundopsicologos.com/centros/centro-de-psicologia-jara-amaro">Mundo psicólogos</a></li>
+                            <li><a href="https://www.mundopsicologos.com/centros/centro-de-psicologia-jara-amaro"><img className="footer__logo-mundopsicologos" src={ mundopsicologos } alt="mundo psicólogos"></img></a></li>
                         
                     </ul>
                 </nav>
             </div>
             
             <nav>
-                <ul className="footer__menu-list">
+                <ul className="footer__menu-list container">
                     <li>Consultas</li>
                     <Link className="link" to="/testimonies">
                         <li>Testimonios</li>
@@ -44,11 +47,11 @@ const Footer = () => {
                     <li>Blog</li>
                 </ul>
             </nav>
-            <div>
-                <p>Desarrollo web <a href="https://www.linkedin.com/in/jara-amaro-mazaira-791b17108/">María Mateo</a></p>
-                <p>Redes sociales <a href="https://www.linkedin.com/in/anaamarovazquez/">Ana Amaro</a></p>
-                <p>Copywriter <a href="https://www.linkedin.com/in/sandra-garc%C3%ADa-moreno-45a811176/">Sandra García</a></p>
-                <p>Fotografía <a href="https://www.instagram.com/sara_m.lo.photo/">Sara M</a></p>
+            <div className="footer__workers-list container">
+                <p><a href="https://www.linkedin.com/in/maria-mateo-vazquez/">Desarrollo web María Mateo</a></p>
+                <p><a href="https://www.linkedin.com/in/anaamarovazquez/">Redes sociales Ana Amaro</a></p>
+                <p><a href="https://www.linkedin.com/in/sandra-garc%C3%ADa-moreno-45a811176/">Copywriter Sandra García</a></p>
+                <p><a href="https://www.instagram.com/sara_m.lo.photo/">Fotografía Sara M</a></p>
 
             </div> 
         </footer>
